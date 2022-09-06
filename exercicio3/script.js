888ac8c25bd2d9185e606d3852cab44a51dd652c
DanielEmidio1988 <daniel.emidio.silva@gmail.com>
2022-09-06 15:59:01 -0300

    Entrega exercicio A012 - 06/09

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,7 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Digite aqui sua nacionalidade").toLocaleLowerCase()
+
+if (nacionalidade === "brasileira" || nacionalidade === "argentina" || nacionalidade === "uruguaia" || nacionalidade === "chilena" || nacionalidade === "colombiana"){
+    console.log("Então você é ", nacionalidade)
+}else{
+    console.log("Nacionalidade não encontrada")
+}
